912814191cadd711f5c489adf6d8465510637c75
Hailin <hailin545@qq.com>
2021-11-02 21:16:15 +0800

    finish 2th  project

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 ## HTML + CSS + JavaScript 练手的50个项目
 
+
+### 2. Progress Steps
+
+* start time: 2021/11/02
+* finish time: 2021/11/02
+* learn:
+    1. css 变量 , var(name, value)
+    2. 伪类，伪元素
+    3. transform： 旋转，缩放，平移，倾斜等
+
+
 ### 1. Expanding Card
 * start time: 2021/11/01
 * finish time: 2021/11/02
 * learn: 
-1. transition: perperty, duration, timing-function, delay;
-2. css选择器 .className .className 区别于 .className.className; 有无空格
-3. 伪类选择器nth-of-type和 nth-child
-4. js中DOM时间监听程序： addEventListener and removeEventListener
+    1. transition: perperty, duration, timing-function, delay;
+    2. css选择器 .className .className 区别于 .className.className; 有无空格
+    3. 伪类选择器nth-of-type和 nth-child
+    4. js中DOM时间监听程序： addEventListener and removeEventListener
+    5. cursor: pointer
+    6. element.classList.add/remove
---
+++ progress-steps/script.js
@@ -0,0 +1,44 @@
+
+
+const prevBtn = document.querySelector('.prev')
+const nextBtn = document.querySelector('.next')
+console.log(nextBtn);
+const circles = document.querySelectorAll('.circle')
+const colorlines = document.querySelectorAll('.colorline');
+var step = 1
+
+prevBtn.addEventListener('click', clickPrev)
+nextBtn.addEventListener('click', clickNext)
+
+
+function clickNext() {
+    if (step < 4) {
+        circles[step].classList.add('active')
+        colorlines[step - 1].classList.add('active')
+        step++
+        if (step == 4) {
+            nextBtn.disabled = true
+            nextBtn.classList.add('notclick')
+        }
+        if (step == 2) {
+            prevBtn.disabled = false
+            prevBtn.classList.remove('notclick')
+        }
+    }
+}
+
+function clickPrev() {
+    if (step > 1) {
+        circles[step - 1].classList.remove('active')
+        colorlines[step - 2].classList.remove('active')
+        step--
+        if (step == 3) {
+            nextBtn.disabled = false;
+            nextBtn.classList.remove('notclick')
+        }
+        if (step == 1) {
+            prevBtn.disabled = true
+            prevBtn.classList.add('notclick')
+        }
+    }
+}
