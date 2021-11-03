8685c6f7c9dd97e79dfd7bfbe9b28ecd0ce82a78
Hailin <hailin545@qq.com>
2021-11-03 11:30:59 +0800

    finish day4

---
+++ hidden-search-widget/script.js
@@ -0,0 +1,10 @@
+
+
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
+
+btn.addEventListener('click', () => {
+
+    input.classList.toggle('hidden');
+    input.focus();
+})
