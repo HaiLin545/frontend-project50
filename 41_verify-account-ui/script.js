e577a1d26dce979dfdd3f3e7338a90fc485a8fca
Hailin <hailin545@qq.com>
2022-12-03 22:44:31 +0800

    41和42顺序纠正；完成41

---
+++ 41_verify-account-ui/script.js
@@ -0,0 +1,20 @@
+const codes = document.querySelectorAll("input");
+
+codes[0].focus();
+
+codes.forEach((code, idx) => {
+    code.addEventListener("keydown", (e) => {
+        if (e.key >= 0 && e.key <= 9) {
+            codes[idx].value = "";
+            if (idx < 5) {
+                setTimeout(() => codes[idx + 1].focus(), 10);
+            }
+        } else if (e.key === "Backspace") {
+            if (idx > 0) {
+                setTimeout(() => codes[idx - 1].focus(), 10);
+            } else {
+                setTimeout(() => codes[0].focus(), 10);
+            }
+        }
+    });
+});
